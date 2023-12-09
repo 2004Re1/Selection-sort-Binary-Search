77750425a63af6fbad375ab5392368f9fc0bf934
Re1 <118055755+2004Re1@users.noreply.github.com>
2023-12-09 23:29:31 +0400

    Pointer

---
+++ Searching/FileName.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int sorting()
+{
+
+
+
+	return 0;
+
+}
+
+
+int main()
+{
+	int a[10] = { 12,23,43,543,543,32,57,325,235,111 };
+	int a_legnth = sizeof(a)/sizeof(a[0]);
+	std::cout << a_legnth;
+
+
+
+
+
+}
