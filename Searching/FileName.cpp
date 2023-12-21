06161be7077fdc9771305d2b7f7e7959f70e7769
Re1 <118055755+2004Re1@users.noreply.github.com>
2023-12-21 20:58:24 +0400

    Update FileName.cpp

--- Searching/FileName.cpp
+++ Searching/FileName.cpp
@@ -47,8 +47,26 @@ int sorting(int b[], int legnth)
 }
 
 
-int Binary_Search()
+int Binary_Search(int arr[],int first_index,int last_index,int goal)
 {
+
+	int middle;
+
+
+	while (first_index<=last_index)
+	{         
+		middle = (last_index + first_index) / 2;
+
+
+		if (goal == arr[middle])
+			return middle;
+
+		else if (goal > arr[middle])
+			first_index = middle + 1;
+		else if (goal < arr[middle])
+			last_index = middle - 1;
+
+	}
 
 
 
@@ -58,26 +76,53 @@ int Binary_Search()
 
 int main()
 {
-	int a[] = { 1 };
 	int a_legnth;
+	int a[100];
 
+
+	//asking for numbers
 	std::cout << "enter size of your array" << std::endl;
 	std::cin >> a_legnth;
+
 
 	for (int i = 0;i < a_legnth;i++)
 		std::cin >> a[i];
 
-	std::cout << std::endl << "your array is" << std::endl;
+	std::cout << std::endl;
+
+
+	//showing them
+	std::cout << "your array is" << std::endl;
 
 	for (int i = 0;i < a_legnth;i++)
 		std::cout << a[i] << " ";
 
-	std::cout << "first we will sort" << std::endl;
+
+	//sorting
+	std::cout<<std::endl << "first we will sort" << std::endl;
 
 	sorting(a, a_legnth);
 
 	for (int i = 0;i < a_legnth;i++)
 		std::cout << a[i] << " ";
+
+	std::cout << std::endl;
+	//variable for search
+
+	std::cout << "choose number from the array to search for it" << std::endl;
+	int target;
+	std::cin >> target;
+	int f_index = 0;
+	int l_index = a_legnth - 1;
+
+	if (Binary_Search(a, f_index, l_index, target)>0)
+		std::cout << "your number is found" << std::endl;
+
+	else
+		std::cout << "your number isn't here";
+	
+
+	
 
 
 
